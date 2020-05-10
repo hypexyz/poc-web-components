6038ebe21416f53f2afc90fb78c370c6d017ccca
Marko Arsic <marsicdev@gmail.com>
2020-05-11 00:31:57 +0200

    Fix stencil components name

--- packages/component-library/stencil.config.ts
+++ packages/component-library/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core'
 
 export const config: Config = {
-    namespace: 'poc-insight',
+    namespace: 'component-library',
     taskQueue: 'async',
     outputTargets: [
         {
